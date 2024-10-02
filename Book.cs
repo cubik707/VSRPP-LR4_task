08fc536dd9a24fd3a7bda9972f00d21cabef6e31
Vladislava <stdemidovez@gmail.com>
2024-10-02 18:08:23 +0300

    class Books

---
+++ Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR4_task
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+    }
+}
